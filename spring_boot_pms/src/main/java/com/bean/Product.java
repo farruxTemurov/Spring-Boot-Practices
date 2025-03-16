1c83428cd8d65fa7218c8c6ac7d892c23cc8a97b
Farrux Temurov <tfarrux70@gmail.com>
2025-03-16 15:30:17 +0500

    Product class updated

--- spring_boot_pms/src/main/java/com/bean/Product.java
+++ spring_boot_pms/src/main/java/com/bean/Product.java
@@ -13,7 +13,7 @@ import jakarta.persistence.Id;
 @Scope("prototype")
 public class Product {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // it helps to make the field auto_increment
 	private int id;
 	private String name;
 	private float price;

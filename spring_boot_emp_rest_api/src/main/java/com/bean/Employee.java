bd50c9f1015cb8604bd7d7f8173c6dbc66497a14
Farrux Temurov <tfarrux70@gmail.com>
2025-03-22 12:10:07 +0500

    worked on service and repository layers

--- spring_boot_emp_rest_api/src/main/java/com/bean/Employee.java
+++ spring_boot_emp_rest_api/src/main/java/com/bean/Employee.java
@@ -1,27 +1,44 @@
 package com.bean;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Component
+@Scope("prototype")
+@Table(name = "employees")
 public class Employee {
+	@Id
 	private int id;
 	private String name;
 	private float salary;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public float getSalary() {
 		return salary;
 	}
+
 	public void setSalary(float salary) {
 		this.salary = salary;
 	}
 
-	
 }

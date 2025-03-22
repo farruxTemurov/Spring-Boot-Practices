97850d99bfa9eeb1f7e838d75a69434525655886
Farrux Temurov <tfarrux70@gmail.com>
2025-03-22 12:22:05 +0500

    worked on controller

---
+++ spring_boot_emp_rest_api/src/main/java/com/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bean.Employee;
+
+import com.service.EmployeeService;
+
+public class EmployeeController {
+	@Autowired
+	EmployeeService employeeService;
+
+	// http://localhost:8080/findAllEmployee
+
+	@RequestMapping(value = "findAllEmployee", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Employee> getAllEmployees() {
+		return employeeService.findAllEmployees();
+	}
+}
